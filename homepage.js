db6e62cd9cd3da048193c8a9d54db8380e6ec9ec
mfux <maxfuchs@protonmail.com>
2022-04-13 23:07:10 +0200

    Using lists

--- homepage.js
+++ homepage.js
@@ -16,12 +16,16 @@ return <h1>{props.title}</h1>
 }
 
 function HomePage() {
-return (
-    <div>
-    {/* Nesting the Header component */}
-    <Header title="React 🥶 "/>
-    </div>
-)
-}
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton']
+  
+    return (
+      <div>
+        <Header title="Develop. Preview. Ship. 🚀" />
+        <ul>
+          {[<li>Ada Lovelace</li>, <li>Grace Hopper</li>, <li>Margaret Hamilton</li>]}
+        </ul>
+      </div>
+    )
+  }
 
 ReactDOM.render(<HomePage />, app)
